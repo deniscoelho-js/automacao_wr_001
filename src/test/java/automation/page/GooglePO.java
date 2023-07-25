aca6bfa837d8eca58cacd66694f6a6ab401456a1
Denis Coelho <deniscoelho.dev@gmail.com>
2023-07-25 15:40:44 -0300

    atualizacao

--- src/test/java/automation/page/GooglePO.java
+++ src/test/java/automation/page/GooglePO.java
@@ -1,5 +1,6 @@
 package automation.page;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -9,10 +10,18 @@ public class GooglePO extends BasePO{
     @FindBy(name = "q")
     public WebElement inputPesquisa;
 
-    @FindBy(id = "results-stats")
+    @FindBy(id = "result-stats")
     public WebElement divResultadoPesquisa;
 
-    protected GooglePO(WebDriver driver) {
+    public GooglePO(WebDriver driver) {
         super(driver);
+    }
+
+    public void pesquisar(String texto){
+        inputPesquisa.sendKeys(texto + Keys.ENTER);
+    }
+
+    public String obterResultadoDaPesquisa(){
+        return divResultadoPesquisa.getText();
     }
 }
--- src/test/java/automation/test/GoogleTest.java
+++ src/test/java/automation/test/GoogleTest.java
@@ -1,25 +1,25 @@
 package automation.test;
 
+import automation.page.GooglePO;
+import org.junit.*;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
 
 public class GoogleTest extends BaseTest {
 
-    //16:28
+    private static GooglePO googlePage;
     @BeforeClass
     public static void prepararTestes(){
-
+        googlePage = new GooglePO(driver);
     }
 
     @Test
     public void devePesquisarNoGoogle() {
-        WebElement inputPesquisa = driver.findElement(By.name("q"));
-        inputPesquisa.sendKeys("Batata frita" + Keys.ENTER);
 
-        String resultado = driver.findElement(By.id("result-stats")).getText();
+        googlePage.pesquisar("Batata frita");
+        String resultado = googlePage.obterResultadoDaPesquisa();
+        Assert.assertTrue(resultado, resultado.contains("Aproximadamente"));
 
         driver.close();
     }
